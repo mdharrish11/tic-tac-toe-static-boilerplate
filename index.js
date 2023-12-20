0f0b4da8ba63120b5b6ae0802b435ef53d4ab277
mdharrish11 <147710217+mdharrish11@users.noreply.github.com>
2023-12-20 11:41:49 +0530

    chgn

--- index.js
+++ index.js
@@ -1,82 +1,78 @@
-const box = document.getElementsByClassName("box");
+let box = document.getElementsByClassName("box")
 
-const result = document.getElementById("result");
+const result = document.getElementById("result")
 
-const message = document.getElementById("message");
+const message = document.getElementById("message")
 
 const button = document.getElementById("button")
 
-
-button.onclick = function(){
-    window.location.reload();
+button.onclick=function(){
+    window.location.reload()
 }
-
-
-
-
 
 for(let i=0;i<box.length;i++){
-    box[i].addEventListener("click",()=>{handleText(i)})
+    box[i].addEventListener("click" , ()=>{
+        handleText(i);
+    });
 }
 
-
-let finalResult = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
+let finalResult = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 
 let xArray = []
-let yArray= []
-let turn ="X";
-let count = 0 ;
+let yArray = []
+
+let turn = "X"
+let count = 0;
+
+
 function handleText(index){
-    if(box[index].innerHTML==""){
-    if(count%2==0){
-
-        box[index].innerHTML=`<h1>${turn}</h1>`;
-        xArray.push(index);
-        turn ="O";
-        checkWinningCombination(finalResult,xArray,turn);
-
-    }else if(count%2!=0){
-
-        box[index].innerHTML = `<h1>${turn}</h1>`;
-        yArray.push(index);
-        turn ="X";
-        checkWinningCombination(finalResult,yArray,turn);
-        
+    if(box[index].innerHTML == ""){
+        if(count%2==0){
+            box[index].innerHTML = `<h1>${turn}</h1>`
+            xArray.push(index)
+            turn = "O"
+            chackWinningCombination(finalResult,xArray,turn)
+          
+        }else if(count%2!=0){
+            box[index].innerHTML = `<h1>${turn}</h1>`
+            yArray.push(index)
+            turn = "X"
+            chackWinningCombination(finalResult,yArray,turn)
+          
+        }
+        // checkLogic();
+    
     }
-    // checkLogic();
     count++;
-    }
 }
 
-let flag = true;
+let flag =  true
+
+// let finalResult = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+
+function chackWinningCombination(finalResult,array,turn){
+
+let gameArray = [];
+for(let i=0;i<finalResult.length;i++){
 
 
-function checkWinningCombination(finalResult,array,turn){
-
-    let gameArray = [];
-
-    for(let i=0; i<finalResult.length;i++){
-        if(Array.isArray(finalResult[i])){
-            checkWinningCombination(finalResult[i],array,turn)
+    if(Array.isArray(finalResult[i])){
+        chackWinningCombination(finalResult[i],array,turn)
+    }else{
+        if(array.includes(finalResult[i])){
+            gameArray.push(true)
         }else{
-            if(array.includes(finalResult[i])){
-                gameArray.push(true);
-            }else{
-                gameArray.push(false);
-            }
+            gameArray.push(false)
         }
     }
-
-    if(gameArray.every((el)=>el==true) && gameArray.length==3){
-
-        result.style.visibility = "visible";
-        message.innerText = `${turn=="X"?"O":"X"} Won the game`;
-        flag=flase;
     }
+if(gameArray.every((el)=>el==true) && gameArray.length==3){
 
-
-    else if(count==8 && flag==true){
-        result.style.visibility = "visible";
-        message.innerText = "The game is tie"
-    }
-  }
+    result.style.visibility = "visible"
+    message.innerText = `${turn=="X"?"O":"X"} won`
+    flag = false
+}else if(count==8 && flag==true){
+    result.style.visibility = "visible"
+    message.innerText = `Its a Tie`
+}
+}
